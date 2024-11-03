658994714fa2aec734027f88513743f1391fcdae
Sebastian S Schütze <sebastian.s.schuetze@deutschebahn.com>
2024-11-04 00:14:15 +0100

    restart application

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,33 +11,39 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-logo.png')}
+          source={require('@/assets/images/partial-react-logo.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Get your Lips fixed!</ThemedText>
+        <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Get a sightseeing in Berlin</ThemedText>
+        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Your tour guide <ThemedText type="defaultSemiBold">Sebastian</ThemedText> will show you Berlin.
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Press{' '}
+          <ThemedText type="defaultSemiBold">
+            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
+          </ThemedText>{' '}
+          to open developer tools.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Fly</ThemedText>
+        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
         <ThemedText>
-          Fly to Turkey to the beautiful town of Adıyaman.
+          Tap the Explore tab to learn more about what's included in this starter app.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: New lips</ThemedText>
+        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
-          Get your operation. Once your ready your new lips will{' '}
-          <ThemedText type="defaultSemiBold">help tourists</ThemedText> to float on the ocean,{' '}
-          <ThemedText type="defaultSemiBold">collect more food</ThemedText> with your new mouth pockets and{' '}
-          <ThemedText type="defaultSemiBold">have a better life</ThemedText>.
+          When you're ready, run{' '}
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -55,10 +61,10 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 101,
-    width: 310,
-    bottom: 50,
-    left: 25,
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
     position: 'absolute',
   },
 });
